f3945d0b2f8a0df48728448ee2511ee2eb257d43
Jude Ogboru <ogboruj@outlook.com>
2023-01-17 11:32:39 +0100

    Implemented Type and Color enums for ChessPiece

--- main/java/com/chess/Chess.java
+++ main/java/com/chess/Chess.java
@@ -24,30 +24,54 @@ public class Chess extends GameManager {
         ChessPiece.setMaxPos(new Vector2(7, 7).mul(gridSize));
         for (int i = 0; i < 16; i++) {
             if (i < 8) {
-                white.add(i, new ChessPiece("/White/pawn.png", new Coordinate(i * gridSize, 6 * gridSize)));
-                black.add(i, new ChessPiece("/Black/pawn1.png", new Coordinate(i * gridSize, gridSize)));
+                white.add(i, new ChessPiece("/White/pawn.png",
+                        new Coordinate(i * gridSize, 6 * gridSize),
+                        ChessPiece.Type.PAWN, ChessPiece.Color.WHITE));
+                black.add(i, new ChessPiece("/Black/pawn1.png",
+                        new Coordinate(i * gridSize, gridSize),
+                        ChessPiece.Type.PAWN, ChessPiece.Color.BLACK));
             } else if (i == 8 || i == 15) {
-                white.add(i, new ChessPiece("/White/rook.png", new Coordinate((i - 8) * gridSize, 7 * gridSize)));
-                black.add(i, new ChessPiece("/Black/rook1.png", new Coordinate((i - 8) * gridSize, 0)));
+                white.add(i, new ChessPiece("/White/rook.png",
+                        new Coordinate((i - 8) * gridSize, 7 * gridSize),
+                        ChessPiece.Type.ROOK, ChessPiece.Color.WHITE));
+                black.add(i, new ChessPiece("/Black/rook1.png",
+                        new Coordinate((i - 8) * gridSize, 0),
+                        ChessPiece.Type.ROOK, ChessPiece.Color.BLACK));
             } else if (i == 9 || i == 14) {
-                white.add(i, new ChessPiece("/White/knight.png", new Coordinate((i - 8) * gridSize, 7 * gridSize)));
-                black.add(i, new ChessPiece("/Black/knight1.png", new Coordinate((i - 8) * gridSize, 0)));
+                white.add(i, new ChessPiece("/White/knight.png",
+                        new Coordinate((i - 8) * gridSize, 7 * gridSize),
+                        ChessPiece.Type.KNIGHT, ChessPiece.Color.WHITE));
+                black.add(i, new ChessPiece("/Black/knight1.png",
+                        new Coordinate((i - 8) * gridSize, 0),
+                        ChessPiece.Type.KNIGHT, ChessPiece.Color.BLACK));
             } else if (i == 10 || i == 13) {
-                white.add(i, new ChessPiece("/White/bishop.png", new Coordinate((i - 8) * gridSize, 7 * gridSize)));
-                black.add(i, new ChessPiece("/Black/bishop1.png", new Coordinate((i - 8) * gridSize, 0)));
+                white.add(i, new ChessPiece("/White/bishop.png",
+                        new Coordinate((i - 8) * gridSize, 7 * gridSize),
+                        ChessPiece.Type.BISHOP, ChessPiece.Color.WHITE));
+                black.add(i, new ChessPiece("/Black/bishop1.png",
+                        new Coordinate((i - 8) * gridSize, 0),
+                        ChessPiece.Type.BISHOP, ChessPiece.Color.BLACK));
             } else if (i == 11) {
-                white.add(i, new ChessPiece("/White/queen.png", new Coordinate((i - 8) * gridSize, 7 * gridSize)));
-                black.add(i, new ChessPiece("/Black/queen1.png", new Coordinate((i - 8) * gridSize, 0)));
+                white.add(i, new ChessPiece("/White/queen.png",
+                        new Coordinate((i - 8) * gridSize, 7 * gridSize),
+                        ChessPiece.Type.QUEEN, ChessPiece.Color.WHITE));
+                black.add(i, new ChessPiece("/Black/queen1.png",
+                        new Coordinate((i - 8) * gridSize, 0),
+                        ChessPiece.Type.QUEEN, ChessPiece.Color.BLACK));
             } else {
-                white.add(i, new ChessPiece("/White/king.png", new Coordinate((i - 8) * gridSize, 7 * gridSize)));
-                black.add(i, new ChessPiece("/Black/king1.png", new Coordinate((i - 8) * gridSize, 0)));
+                white.add(i, new ChessPiece("/White/king.png",
+                        new Coordinate((i - 8) * gridSize, 7 * gridSize),
+                        ChessPiece.Type.KING, ChessPiece.Color.WHITE));
+                black.add(i, new ChessPiece("/Black/king1.png",
+                        new Coordinate((i - 8) * gridSize, 0),
+                        ChessPiece.Type.KING, ChessPiece.Color.BLACK));
             }
         }
     }
 
     private int selectedIndex = -1;
     private int plays = 0;
-    private Vector2 prevPos;
+    private Vector2 selectedPos;
 
     public void onUpdate(double d) {
         Vector2 mouse = new Vector2(input.getMousePos());
@@ -56,16 +80,17 @@ public class Chess extends GameManager {
             for (int i = 0; i < (plays % 2 == 0 ? white : black).size(); i++) {
                 if (new Vector2((plays % 2 == 0 ? white : black).get(i).getPos()).equals(mouse)) {
                     selectedIndex = i;
-                    prevPos = new Vector2((plays % 2 == 0 ? white : black).get(i).getPos());
+                    selectedPos = new Vector2((plays % 2 == 0 ? white : black).get(i).getPos());
                     break;
                 }
             }
         } else if (input.isButtonPressed(1) && (selectedIndex >= 0 || selectedIndex < (plays % 2 == 0 ? white : black).size())) {
             mouse = mouse.sub(mouse.mod(gridSize));
+
             (plays % 2 == 0 ? white : black).get(selectedIndex).setPos(mouse);
-            if (!prevPos.equals(mouse)) {
+            if (!selectedPos.equals(mouse)) {
                 plays++;
-                prevPos = null;
+                selectedPos = null;
             }
             selectedIndex = -1;
         }
@@ -74,17 +99,17 @@ public class Chess extends GameManager {
         }
         if (input.isKeyPressed(KeyEvent.VK_ESCAPE)) {
             quit();
+            System.exit(0);
         }
     }
 
     public void onRender() {
         drawBoard(0, 0, 8, 8, gridSize, 0xff6d3e17, 0xfff0c380);
-        for (ChessPiece piece : white) {
-            renderer.drawImage(piece.getImage(), piece.getPos().getX(), piece.getPos().getY());
+        if (selectedIndex >= 0 && selectedPos != null) {
+            renderer.fillRectangle(selectedPos.getX(), selectedPos.getY(), gridSize, gridSize, plays % 2 == 0 ? 0xffff4040 : 0xff0080ff);
         }
-        for (ChessPiece piece : black) {
-            renderer.drawImage(piece.getImage(), piece.getPos().getX(), piece.getPos().getY());
-        }
+        for (ChessPiece piece : white) renderer.drawImage(piece.getImage(), piece.getPos().getX(), piece.getPos().getY());
+        for (ChessPiece piece : black) renderer.drawImage(piece.getImage(), piece.getPos().getX(), piece.getPos().getY());
     }
 
     public static void main(String[] args) {
--- main/java/com/chess/ChessPiece.java
+++ main/java/com/chess/ChessPiece.java
@@ -5,13 +5,39 @@ import com.crossly.gfx.Image;
 import com.crossly.utils.Coordinate;
 
 public class ChessPiece {
+    public enum Color {
+        WHITE, BLACK;
+    }
+    public enum Type {
+        PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING;
+        private String whitePath;
+        private String blackPath;
+        public void setBlackPath(String blackPath) {
+            this.blackPath = blackPath;
+        }
+        public String getBlackPath() {
+            return blackPath;
+        }
+        public void setWhitePath(String whitePath) {
+            this.whitePath = whitePath;
+        }
+        public String getWhitePath() {
+            return whitePath;
+        }
+    }
     private Image image;
     private Coordinate pos;
+    private Type type;
+    private Color color;
     private static Coordinate maxPos = new Coordinate();
     private static Coordinate minPos = new Coordinate();
-    public ChessPiece(String imagePath, Coordinate position) {
+    public ChessPiece(String imagePath, Coordinate position, Type type, Color color) {
         image = new Image(imagePath);
         pos = position;
+        if (color == Color.WHITE) type.setWhitePath(imagePath);
+        else type.setBlackPath(imagePath);
+        this.type = type;
+        this.color = color;
     }
 
     public static void setMinPos(Coordinate pos) {
@@ -37,5 +63,23 @@ public class ChessPiece {
 
     public Vector2 getImageSize() {
         return new Vector2(image.getWidth(), image.getHeight());
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+        image = new Image(color == Color.WHITE ? type.getWhitePath() : type.getBlackPath());
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        setType(type);
     }
 }
