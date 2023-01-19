a4b44e8a25c98d41191583b255d049a845285a00
Jude Ogboru <ogboruj@outlook.com>
2023-01-19 12:44:23 +0100

    Changed equals function and added compareTo and hashCode functions

--- main/java/com/chess/utils/Vector2.java
+++ main/java/com/chess/utils/Vector2.java
@@ -2,7 +2,7 @@ package com.chess.utils;
 
 import com.crossly.utils.Coordinate;
 
-public class Vector2 extends Coordinate {
+public class Vector2 extends Coordinate implements Comparable<Vector2> {
     public Vector2() {
         super();
     }
@@ -43,9 +43,23 @@ public class Vector2 extends Coordinate {
          return new Vector2(this.getX() % scalar, this.getY() % scalar);
     }
 
-    public boolean equals(Object o) {
-        if (!o.getClass().equals(this.getClass())) return false;
-        Vector2 that = (Vector2)o;
-        return this.getX() == that.getX() && this.getY() == that.getY();
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public int compareTo(Vector2 o) {
+        if (this.equals(o)) return 0;
+        else {
+            int thisMagSqr = getX() * getX() + getY() * getY();
+            int thatMagSqr = o.getX() * o.getX() + o.getY() * o.getY();
+            return (thisMagSqr > thatMagSqr ? 1 : -1);
+        }
     }
 }
